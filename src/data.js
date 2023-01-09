f25c6b92b101d532e6808bdbac9d3eb511959c35
Muhsin FE_Team_Rookie <muhsin.altintop@gmail.com>
2023-01-09 20:24:50 +0300

    a react shopping application copied from github.

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const list = [
+    {
+      id: 1,
+      title: "Apple",
+      author: "by Wonder House Books | 25 April 2018",
+      price: 50,
+      img: "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
+    },
+    {
+      id: 2,
+      title: "Banana",
+      author: "by Maple Press  | 1 September 2020",
+      price: 30,
+      img: "https://www.collinsdictionary.com/images/full/banana_64728013.jpg",
+    },
+    {
+      id: 3,
+      title: "Cherry",
+      author: "by Om Books Editorial Team  | 25 November 2018",
+      price: 20,
+      img: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Cherry_Stella444.jpg",
+    },
+    {
+      id: 4,
+      title: "Date",
+      author: "by Wonder House Books | 25 April 2018",
+      price: 15,
+      img: "https://m.media-amazon.com/images/I/81Gbz0XnW7L._AC_UY327_FMwebp_QL65_.jpg",
+    },
+];
+
+export default list;
